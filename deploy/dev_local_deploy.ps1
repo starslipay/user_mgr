033fb6c1caf5e5c7a71108e1093b4a465b634757
starsli <starsli@tencent.com>
2026-07-12 13:06:18 +0800

    save

--- deploy/dev_local_deploy.ps1
+++ deploy/dev_local_deploy.ps1
@@ -1,4 +1,4 @@
-$VERSION = "v1.0.0"
+$VERSION = "v1.0.8"
 
 pushd ..
 
@@ -6,7 +6,7 @@ pushd ..
 docker rm -f user_mgr
 docker rmi -f user_mgr:$VERSION
 docker build -t user_mgr:$VERSION .
-docker run -d --name user_mgr -p 30880:8080 user_mgr:$VERSION
+docker run -d --name user_mgr --network local_deps_install_dev_net -p 30880:8080 user_mgr:$VERSION
 docker ps
 docker logs user_mgr -f
 
