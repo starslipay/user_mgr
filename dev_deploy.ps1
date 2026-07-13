f00a4b0cd18528968f75c23f7c2668bfbbb1c1ad
starsli <starsli@tencent.com>
2026-07-13 23:01:52 +0800

    deploy

---
+++ dev_deploy.ps1
@@ -0,0 +1,10 @@
+$MODULE_NAME = "user_mgr"
+$VERSION = "v1.0.0"
+$IMAGE_NAME = "${MODULE_NAME}:${VERSION}"
+
+docker rm -f $MODULE_NAME
+docker rmi -f $IMAGE_NAME
+docker build -t $IMAGE_NAME .
+docker run -d --name $MODULE_NAME --network local_deps_install_dev_net -p 30880:8080 $IMAGE_NAME
+docker ps
+docker logs $MODULE_NAME
