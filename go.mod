aa529ff24e7e93ce9b891e2f44ee29faf7b6c06b
starsli <starsli@tencent.com>
2026-07-27 15:18:45 +0800

    错误码处理

--- go.mod
+++ go.mod
@@ -55,7 +55,7 @@ require (
 	github.com/prometheus/procfs v0.16.1 // indirect
 	github.com/redis/go-redis/v9 v9.19.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
-	github.com/starslipay/paycomm v1.0.0 // indirect
+	github.com/starslipay/paycomm v1.0.1 // indirect
 	github.com/titanous/json5 v1.0.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.21 // indirect
--- internal/logic/reguserlogic.go
+++ internal/logic/reguserlogic.go
@@ -154,7 +154,7 @@ func (l *RegUserLogic) RegUser(in *user_mgr_pb.RegUserReq) (*user_mgr_pb.RegUser
 		CurType: 1, // 1-人民币
 	})
 	if err != nil {
-		return nil, xerr.ParseRPCError(err)
+		return nil, xerror.HandleRPCError(err, "account_mgr.CreateAccount")
 	}
 	if createAccountRsp.IsRepeat {
 		l.Logger.Info("create account already exist, create repeat")
@@ -177,7 +177,7 @@ func (l *RegUserLogic) RegUser(in *user_mgr_pb.RegUserReq) (*user_mgr_pb.RegUser
 func (l *RegUserLogic) genUid() (int64, error) {
 	genUidRsp, err := l.svcCtx.TradeIdMgrRpcClient.GenUid(l.ctx, &trade_id_mgr_pb.GenUidReq{})
 	if err != nil {
-		return 0, xerr.ParseRPCError(err)
+		return 0, xerror.HandleRPCError(err, "trade_id_mgr.GenUid")
 	}
 	return genUidRsp.Uid, nil
 }
